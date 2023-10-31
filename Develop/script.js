1adfb22dbfb24d2652d371fff70b12402f3c6746
Diana Tran <diana.tran44@gmail.com>
2023-10-31 20:56:32 +1100

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,9 @@
-// Assignment code here
+// Variable defintions
+
+      var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+      var uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var numericChars = '0123456789';
+      var specialChars = '!@#$%^&*()_+{}[]|:;"<>,.?/~';
 
 // Get the button element from the HTML
 var button = document.querySelector('button');
@@ -8,6 +13,18 @@ button.addEventListener('click', function() {
   // Display the prompt when the button is clicked
   var userInput = prompt('What is the length of your password? (Min 8 Max 126)');
   console.log(userInput);
+
+
+
+  var userInput1 = confirm('Do you want to include lowercase?');
+  console.log(userInput1);
+  var userInput2 = confirm('Do you want to include uppercase?');
+  console.log(userInput2);
+  var userInput3 = confirm('Do you want to include numeric?');
+  console.log(userInput3);
+  var userInput4 = confirm('Do you want to include special characters?');
+  console.log(userInput4);
+ 
 });
 
 
