324b3b38980d3964043920099bd288aef03a57a9
Diana Tran <diana.tran44@gmail.com>
2023-11-02 19:49:52 +1100

    added comments

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,6 @@ var numeric = '0123456789';
 var special = '@%+\\/' + "'!#$^?:,)(}{][~`-_.";
 
 
-
 // Get the button element from the HTML
 var generateBtn = document.querySelector("#generate");
 
@@ -19,21 +18,21 @@ function writePassword() {
   var passwordLength = parseInt(prompt('What is the length of your password? (Min 8 Max 128)'));
 
   // If password length is not a number or does not meet min & max length, show an alert to the user and return
-  if (isNaN(passwordLength) || passwordLength<8 || passwordLength>128 ) 
-  {window.alert("Password does not meet requirements. Try again"); 
+  if (isNaN(passwordLength) || passwordLength<8 || passwordLength>128 ) {
+    window.alert("Password does not meet requirements. Try again"); 
     return;
   }
   // If password length is a number or meets the min & max length, ask the user for preference of lowercase, uppercase, numeric and/or special characters
-  else 
-  {var userInputLower = confirm('Do you want to include lowercase?')
+  else {
+  var userInputLower = confirm('Do you want to include lowercase?')
   var userInputUpper = confirm('Do you want to include uppercase?')
   var userInputNumeric = confirm('Do you want to include numeric?')
   var userInputSpecial = confirm('Do you want to include special characters?')
   }
 
   // If user does not select a preference, show an alert to the user and return
-  if(!userInputLower && !userInputUpper && !userInputNumeric && !userInputSpecial)
-  {window.alert("Please select at lease one preference!"); 
+  if(!userInputLower && !userInputUpper && !userInputNumeric && !userInputSpecial){
+  window.alert("Please select at lease one preference!"); 
   return;
 }
 
@@ -56,13 +55,14 @@ if (userInputSpecial) {
 
 var allChars = array.join('');
 
-// using a for loop - pick up from the concat , password += a
+// Use a loop and math random to create a random password based on the array
 var password = '';
 for (var i = 0; i < passwordLength; i++) {
   var randomIndex = Math.floor(Math.random() * allChars.length);
   password += allChars[randomIndex];
 }
 
+// Show the random password on the screen for the user
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
 
